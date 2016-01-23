19e057ef22932725cf63c7dc29d44dc3022de854
Davi Frossard <davienf1@gmail.com>
2016-01-23 03:09:56 -0500

    Working by using remote files

--- get_data.py
+++ get_data.py
@@ -3,9 +3,23 @@ from multiprocessing import Pool, TimeoutError
 from socket import timeout
 from hashlib import sha256
 from scipy.misc import imread, imsave
+from numpy import dot
+from io import BytesIO
 import urllib2
-import os
-import shutil
+
+import contextlib
+import sys
+import cStringIO
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = cStringIO.StringIO()
+    yield
+    sys.stdout = save_stdout
+
+def rgb2gray(image):
+    return dot(image[...,:3], [0.299, 0.587, 0.114])
 
 def process_item(item):
     try:
@@ -23,86 +37,92 @@ def process_item(item):
             image = urllib2.urlopen(url, timeout=2)
         except urllib2.URLError,e:
             print "[EF] "+url+" ["+filename+"] threw error "+str(e)
-            return 0
+            return [0]
         except timeout:
             print "[TF] "+url+" ["+filename+"] timeouted"
-            return 0
+            return [0]
         image_data = image.read()
 
         # Calculate the hash and check against expected value
         downloaded_hash = sha256(image_data).hexdigest()
         if downloaded_hash != imhash:
             print "[HF] "+url+" ["+filename+"] Hash check failed"
-            return 0
+            return [0]
 
         # Crop face and save as greyscale
-        output = open("uncropped/"+filename,"wb")
-        output.write(image_data)
-        output.close()
-        imarray = imread("uncropped/"+filename, 1)
-        face = imarray[facecoord[1]:facecoord[3],facecoord[0]:facecoord[2]]
-        imsave("cropped/"+filename,face)
+        imarray = imread(BytesIO(image_data))
+        face = imarray[facecoord[1]:facecoord[3],facecoord[0]:facecoord[2]].dot([0.299, 0.587, 0.114])
 
         # Everything went as expected
         print "[S] "+url+" ["+filename+"] downloaded"
-        return 1
-    except:
-        print "Thread error"
-        return 0
+        return [1, imarray, face]
+    except Exception,e:
+        print "Thread error: "+e.message
+        return [0]
 
-def fetch_data(source, targets, amount, numthreads=10, threadtimeout=3):
-    data_lines = list([a.split("\t") for a in open(source).readlines()])
+def fetch_data_artist(target_data, target, amount, numthreads=10, threadtimeout=3):
+    download_queue = []
     pool = Pool(processes=numthreads)
-    total_sucess = 0;
-    for target in targets:
-        download_queue = []
+    photos = []
+    faces = []
+    actors = []
+    # Shuffles data from actor
+    shuffle(target_data)
+    if len(target_data) == 0:
+        print target+" not found in the source"
+        return [0]
 
-        # Fetches all lines of data from actor and shuffles it
-        target_data = list([t_data for t_data in data_lines if t_data[0] == target])
-        shuffle(target_data)
-        if len(target_data) == 0:
-            print target+" not found in the source"
-            continue
-        # Create artist's directory, overwrite if existent
-        if os.path.exists("cropped/"+target):
-            shutil.rmtree("cropped/"+target)
-        os.makedirs("cropped/"+target)
-        if os.path.exists("uncropped/"+target):
-            shutil.rmtree("uncropped/"+target)
-        os.makedirs("uncropped/"+target)
+    # Add indexes to entries (favor parallelism)
+    i = 0
+    for data in target_data:
+        data.append(i)
+        download_queue.append(data)
+        i+=1
 
-        # Add indexes to entries (favor parallelism)
-        i = 0
-        for data in target_data:
-            data.append(i)
-            download_queue.append(data)
-            i+=1
-
-        # Only download a determinate amount of images
-        imsuccess=0
-        if amount > 0:
-            last=0
-            while imsuccess < amount:
-                diff = amount - imsuccess
-                processes = [pool.apply_async(process_item, [i]) for i in download_queue[last:last+diff]]
-                last += diff
-                for process in processes:
-                    try:
-                        imsuccess += process.get(timeout=threadtimeout)
-                    except TimeoutError,e:
-                        print "Thread timeout"
-
-        # Download all images
-        else:
-            processes = [pool.apply_async(process_item, [i]) for i in download_queue]
+    # Only download a determinate amount of images
+    imsuccess = 0
+    if amount > 0:
+        last = 0
+        while imsuccess < amount:
+            diff = amount - imsuccess
+            processes = [pool.apply_async(process_item, [i]) for i in download_queue[last:last+diff]]
+            last += diff
             for process in processes:
                 try:
-                    imsuccess += process.get(timeout=threadtimeout)
+                    retval = process.get(timeout=threadtimeout)[0]
+                    imsuccess += retval
+                    if retval == 1:
+                        photos.append(process.get(timeout=threadtimeout)[1])
+                        faces.append(process.get(timeout=threadtimeout)[2])
+                        actors.append(target)
                 except TimeoutError,e:
                     print "Thread timeout"
-        print "Downloaded %d image[s] of %s" %(imsuccess,target)
-        total_sucess += imsuccess
 
-    return total_sucess
-    # Job finished
-    print "Done"
+    # Download all images
+    else:
+        processes = [pool.apply_async(process_item, [i]) for i in download_queue]
+        for process in processes:
+            try:
+                print process.get(timeout=threadtimeout)
+            except TimeoutError,e:
+                print "Thread timeout"
+    print "Downloaded %d image[s] of %s" %(imsuccess,target)
+    total_success = imsuccess
+    return total_success, photos, faces, actors
+
+
+def fetch_data(source, targets, amount, numthreads=50, threadtimeout=2):
+    data_lines = list([a.split("\t") for a in open(source).readlines()])
+    total_success = 0;
+    photos = []
+    faces = []
+    actors = []
+    for target in targets:
+        target_data = list([t_data for t_data in data_lines if t_data[0] == target])
+        tsuccess, tphotos, tfaces, tactors = fetch_data_artist(target_data, target, amount, numthreads, threadtimeout)
+        total_success += tsuccess
+        photos.extend(tphotos)
+        faces.extend(tfaces)
+        actors.extend(tactors)
+
+    return total_success, photos, faces, actors
