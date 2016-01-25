2c72af541431d227bdbf538dc6c4b2998ac4c90a
Davi Frossard <davienf1@gmail.com>
2016-01-25 04:05:08 -0500

    Added code to compile report

--- faces.py
+++ faces.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
+import tempfile
 import shutil
+from glob import glob
 
 save_ext = raw_input("Enter extension to save graphs [eps by default]: ") or 'eps'
 show_graph = '1' if raw_input("Show graph windows [y/N]? ").lower() == 'y' else '0'
@@ -18,18 +20,29 @@ if os.path.exists("cropped"):
 
 print("\n")
 
-print "RUNNING PART 1"
-subprocess.call("python part1.py %s %s" %(save_ext, show_graph), shell=True)
+# print "RUNNING PART 1"
+# subprocess.call("python part1.py %s %s" %(save_ext, show_graph), shell=True)
+#
+# print "\n\nRUNNING PART 3"
+# subprocess.call("python part3.py %s %s" %(save_ext, show_graph), shell=True)
+#
+# print "\n\nRUNNING PART 4"
+# subprocess.call("python part4.py %s %s" %(save_ext, show_graph), shell=True)
+#
+# print "\n\nRUNNING PART 5"
+# subprocess.call("python part5.py %s %s" %(save_ext, show_graph), shell=True)
+#
+# print "\n\nRUNNING PART 6"
+# subprocess.call("python part6.py %s %s" %(save_ext, show_graph), shell=True)
+# subprocess.call("python part6_2.py %s %s" %(save_ext, show_graph), shell=True)
 
-print "\n\nRUNNING PART 3"
-subprocess.call("python part3.py %s %s" %(save_ext, show_graph), shell=True)
+print "\n\nCOMPILING REPORT"
+current = os.getcwd()
+temp = tempfile.mkdtemp()
+shutil.copy('faces.tex',temp)
+os.chdir(temp)
+subprocess.call(['pdflatex','faces.tex'])
+subprocess.call(['pdflatex','faces.tex'])
+shutil.copy('faces.pdf',current)
+shutil.rmtree(temp)
 
-print "\n\nRUNNING PART 4"
-subprocess.call("python part4.py %s %s" %(save_ext, show_graph), shell=True)
-
-print "\n\nRUNNING PART 5"
-subprocess.call("python part5.py %s %s" %(save_ext, show_graph), shell=True)
-
-print "\n\nRUNNING PART 6"
-subprocess.call("python part6.py %s %s" %(save_ext, show_graph), shell=True)
-subprocess.call("python part6_2.py %s %s" %(save_ext, show_graph), shell=True)
