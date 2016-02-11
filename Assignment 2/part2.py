a353c1974630e8ad84b8df48fe528d85746f248b
Davi Frossard <davienf1@gmail.com>
2016-02-10 21:35:44 -0500

    General feedforward implemented. Should work for any NN topology.

--- Assignment 2/part2.py
+++ Assignment 2/part2.py
@@ -4,11 +4,6 @@ from part1 import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-# images, classes = load_mnist_mat('mnist_all.mat')
-#
-# x_train, t_train, x_validation, t_validation, x_test, t_test = \
-#     make_sets(images, classes, train=40000, validation=10000)
-#
 
 def feed_forward(inputs, weights, biases, functions):
     if not (len(weights) == len(biases) and len(biases) == len(functions)):
@@ -17,8 +12,8 @@ def feed_forward(inputs, weights, biases, functions):
     activations = []
 
     for l in range(layers):
-        activation = functions[l](np.dot(inputs, weights[l]) + biases[l])
-        inputs = activation
+        activation = np.dot(inputs, weights[l]) + biases[l]
         activations.append(activation)
-
+        inputs = functions[l](activation)
+    # del activations[-1]
     return inputs, activations
