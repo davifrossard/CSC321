8564eeb55e9415727ee52ff3c891f24fc58ea589
Davi Frossard <davienf1@gmail.com>
2016-02-11 04:10:03 -0500

    Need to implement derivative of the cost with respect to bias of hidden layer

---
+++ Assignment 2/part7.py
@@ -0,0 +1,39 @@
+from math_funcs import *
+
+import numpy as np
+
+def dCost_dWeight2(x, w, b, y):
+    w2, b2 = w[1], b[1]
+    w1, b1 = w[0], b[0]
+    m = len(y)
+
+    a1 = tanh(x.dot(w1) + b1)
+    z2 = a1.dot(w2) + b2
+
+    delta2 = (softmax(z2)-y)
+    dcdw2 = (1.0/m) * a1.T.dot(delta2)
+    return dcdw2
+
+def dCost_dBias2(x, w, b, y):
+    w2, b2 = w[1], b[1]
+    w1, b1 = w[0], b[0]
+    m = len(y)
+
+    a1 = tanh(x.dot(w1) + b1)
+    z2 = a1.dot(w2) + b2
+    r = (1.0/m) * sum((softmax(z2)-y))
+    return r
+
+def dCost_dWeight1(x, w, b, y):
+    w2, b2 = w[1], b[1]
+    w1, b1 = w[0], b[0]
+    m = len(y)
+
+    z1 = x.dot(w1) + b1
+    z2 = z1.dot(w2) + b2
+
+    delta2 = (softmax(z2)-y)
+
+    delta1 = np.dot(delta2, w2.T) * dtanh(z1)
+    dcdw1 = np.dot(x.T, delta1)
+    return (1.0/m)*dcdw1
