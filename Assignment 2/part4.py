ec6944873095206eb4cbef093c073578b8348918
Davi Frossard <davienf1@gmail.com>
2016-02-10 21:35:05 -0500

    Gradient comparation working using trivial finite differences.

---
+++ Assignment 2/part4.py
@@ -0,0 +1,21 @@
+from part3 import *
+from math_funcs import *
+
+def compare_gradient(x, w, b, funcs, y):
+    gradw = np.sum(dCost_dWeight(x, w[0], b[0], y))
+    gradb = np.sum(dCost_dBias(x, w[0], b[0], y))
+
+    w1 = cross_entropy(x, w, b, funcs, y)[-1]
+    w2 = cross_entropy(x, [w[0]+0.0001], b, funcs, y)[-1]
+    gradw_approx = np.sum((w2-w1)/0.0001)
+
+    b1 = cross_entropy(x, w, b, funcs, y)[-1]
+    b2 = cross_entropy(x, w, [b[0]+0.0001], funcs, y)[-1]
+    gradb_approx = np.sum((b2-b1)/0.0001)
+
+    print "Weight Gradient:",gradw
+    print "Finite Differences:",gradw_approx
+    print "Error:", gradw-gradw_approx,"\n"
+    print "Bias Gradient:",gradb
+    print "Finite Differences:",gradb_approx
+    print "Error:", gradb-gradb_approx
