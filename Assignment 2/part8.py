a067bb0ffebe0698c7aacdbd242556e3d3fd7588
Davi Frossard <davienf1@gmail.com>
2016-02-13 14:40:02 -0500

    All gradients being compared

--- Assignment 2/part8.py
+++ Assignment 2/part8.py
@@ -1,21 +1,82 @@
+from copy import deepcopy
+
 from part7 import *
 from math_funcs import *
 
-def compare_gradient2(x, w, b, funcs, y):
-    gradw2 = dCost_dWeight2(x, w, b, y).sum(axis=0)
-    gradb2 = dCost_dBias2(x, w, b, y)
+def compare_gradient2(x, w, b, y, delta=1e-10, numpoints=3):
+    np.random.seed(0)
+    print "----------------------------------"
+    print "Evaluating Gradients of 2nd Layer:"
+    print "----------------------------------"
 
-    w21 = cross_entropy(x, w, b, funcs, y)
-    w22 = cross_entropy(x, [w[0], w[1]+0.0001], b, funcs, y)
-    gradw2_approx = (w22-w21)/0.0001
+    idw = zip(np.random.choice(len(w[1]), numpoints), np.random.choice(len(w[1][0]), numpoints))
+    idb = np.random.choice(len(b[1]), numpoints)
 
-    b21 = cross_entropy(x, w, b, funcs, y)
-    b22 = cross_entropy(x, w, [b[0], b[1]+0.0001], funcs, y)
-    gradb2_approx = (b22-b21)/0.0001
+    gradsw, gradsb = np.zeros(numpoints), np.zeros(numpoints)
+    b2 = cross_entropy(x, w, b, [tanh, softmax], y)
 
-    print "Weight Gradient:",gradw2
-    print "Finite Differences:",gradw2_approx
-    print "Error:", gradw2-gradw2_approx,"\n"
-    print "Bias Gradient:",gradb2
-    print "Finite Differences:",gradb2_approx
-    print "Error:", gradb2-gradb2_approx
+    print "Evaluating gradient for weights", idw
+    for k, index in enumerate(idw):
+        wl = deepcopy(w)
+        i,j = index
+
+        wl[1][i][j] += delta
+        b1 = cross_entropy(x, wl, b, [tanh, softmax], y)
+        gradsw[k] = (b1-b2)/delta
+
+    gradw = dCost_dWeight2(x, w, b, y)[zip(*idw)]
+    print "Gradient:",gradw
+    print "Finite Differences:",gradsw
+    print "Error:", gradw-gradsw,"\n"
+
+
+    print "Evaluating gradient for bias", idb
+    for k, index in enumerate(idb):
+        bl = deepcopy(b)
+
+        bl[1][index] += delta
+        b1 = cross_entropy(x, w, bl, [tanh, softmax], y)
+        gradsb[k] = (b1-b2)/delta
+
+    gradb = dCost_dBias2(x, w, b, y)[idb]
+    print "Bias Gradient:",gradb
+    print "Finite Differences:",gradsb
+    print "Error:", gradb-gradsb
+
+    print "----------------------------------"
+    print "Evaluating Gradients of 1st Layer:"
+    print "----------------------------------"
+
+    idw = zip(np.random.choice(len(w[0]), numpoints), np.random.choice(len(w[0][0]), numpoints))
+    idb = np.random.choice(len(b[0]), numpoints)
+
+    gradsw, gradsb = np.zeros(numpoints), np.zeros(numpoints)
+
+    print "Evaluating gradient for weights", idw
+    for k, index in enumerate(idw):
+        wl = deepcopy(w)
+        i,j = 350,164
+
+        wl[0][i][j] += delta
+        b1 = cross_entropy(x, wl, b, [tanh, softmax], y)
+        gradsw[k] = (b1-b2)/delta
+
+    gradw = dCost_dWeight1(x, w, b, y)[zip(*idw)]
+    print "Gradient:",gradw
+    print "Finite Differences:",gradsw
+    print "Error:", gradw-gradsw,"\n"
+
+    for k, index in enumerate(idb):
+        bl = deepcopy(b)
+
+        bl[0][index] += delta
+        b1 = cross_entropy(x, w, bl, [tanh, softmax], y)
+        gradsb[k] = (b1-b2)/delta
+
+    gradb = dCost_dBias1(x, w, b, y)[idb]
+
+    print "Evaluating gradient for bias", idb
+    print "Bias Gradient:",gradb
+    print "Finite Differences:",gradsb
+    print "Error:", gradb-gradsb
+
