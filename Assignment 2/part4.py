e4255f7e0026a1dc5c27a9661443e3845bead4c5
Davi Frossard <davienf1@gmail.com>
2016-02-13 14:33:36 -0500

    Changed method to calculate derivatives with respect to a single weight

--- Assignment 2/part4.py
+++ Assignment 2/part4.py
@@ -1,21 +1,42 @@
 from part3 import *
 from math_funcs import *
 
-def compare_gradient(x, w, b, funcs, y):
-    gradw = np.sum(dCost_dWeight(x, w[0], b[0], y))
-    gradb = np.sum(dCost_dBias(x, w[0], b[0], y))
+def compare_gradient(x, w, b, y, delta=1e-5):
+    np.random.seed(0)
+    idw = zip(np.random.choice(len(w), 3), np.random.choice(len(w[0]), 3))
+    idb = np.random.choice(len(b), 3)
 
-    w1 = cross_entropy(x, w, b, funcs, y)
-    w2 = cross_entropy(x, [w[0]+0.0001], b, funcs, y)
-    gradw_approx = np.sum((w2-w1)/0.0001)
+    wd = np.array([w,w,w])
+    bd = np.array([b,b,b])
 
-    b1 = cross_entropy(x, w, b, funcs, y)
-    b2 = cross_entropy(x, w, [b[0]+0.0001], funcs, y)
-    gradb_approx = np.sum((b2-b1)/0.0001)
+    for i, index in enumerate(idw):
+        wd[i, index[0], index[1]] += delta
 
-    print "Weight Gradient:",gradw
-    print "Finite Differences:",gradw_approx
-    print "Error:", gradw-gradw_approx,"\n"
+    for i, index in enumerate(idb):
+        bd[i, index] += delta
+
+    gradsw = np.zeros(3)
+    for i in range(3):
+        w1 = cross_entropy(x, [wd[i]], [b], [softmax], y)
+        w2 = cross_entropy(x, [w], [b], [softmax], y)
+        gradsw[i] = (w1-w2)/delta
+
+    gradsb = np.zeros(3)
+    for i in range(3):
+        b1 = cross_entropy(x, [w], [bd[i]], [softmax], y)
+        b2 = cross_entropy(x, [w], [b], [softmax], y)
+        gradsb[i] = (b1-b2)/delta
+
+    gradw = dCost_dWeight(x, w, b, y)[zip(*idw)]
+    gradb = dCost_dBias(x, w, b, y)[idb]
+
+    print "Evaluating gradient for weights", idw
+    print "Gradient:",gradw
+    print "Finite Differences:",gradsw
+    print "Error:", gradw-gradsw,"\n"
+
+
+    print "Evaluating gradient for bias", idb
     print "Bias Gradient:",gradb
-    print "Finite Differences:",gradb_approx
-    print "Error:", gradb-gradb_approx
+    print "Finite Differences:",gradsb
+    print "Error:", gradb-gradsb
