167db05addff088d324f529fc219335f20dca2e4
Davi Frossard <davienf1@gmail.com>
2016-02-13 14:32:08 -0500

    better variable naming and added outputs

--- Assignment 2/part2.py
+++ Assignment 2/part2.py
@@ -9,11 +9,12 @@ def feed_forward(inputs, weights, biases, functions):
     if not (len(weights) == len(biases) and len(biases) == len(functions)):
         raise ValueError("Inconsistent Model. Different number of weights, biases and functions.")
     layers = len(weights)
+    outputs = []
     activations = []
 
     for l in range(layers):
-        activation = np.dot(inputs, weights[l]) + biases[l]
-        activations.append(activation)
-        inputs = functions[l](activation)
-    # del activations[-1]
-    return inputs, activations
+        output = np.dot(inputs, weights[l]) + biases[l]
+        outputs.append(output)
+        inputs = functions[l](output)
+        activations.append(inputs)
+    return inputs, outputs, activations
