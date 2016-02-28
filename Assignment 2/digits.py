3766cb5f6beec2426c28f5661ea95fc7cd92f28b
Davi Frossard <davienf1@gmail.com>
2016-02-28 01:24:55 -0500

    Added tex compilation and better output for part 8.

--- Assignment 2/digits.py
+++ Assignment 2/digits.py
@@ -11,13 +11,19 @@ from part9 import *
 
 import pickle
 import os
+import tempfile
+import sys
+import subprocess
 import matplotlib.pyplot as plt
 import time, datetime
 
 np.random.seed(0)
 
+if not os.path.exists("results"):
+    os.makedirs("results")
+
 plot = raw_input("Show plots? [y/N]").lower() == 'y'
-ext = raw_input("Extension to save plots? [pdf]") or 'pdf'
+ext = raw_input("Extension to save plots? [PDF/eps/png/jpg/jpeg]").lower() or 'pdf'
 
 print("\n--------------\nRUNNING PART 1\n--------------")
 
@@ -45,12 +51,10 @@ if os.path.exists('logistic_regression.pickle'):
         with open('logistic_regression.pickle') as f:
             w, b, epoch, converged, train_cost, train_accuracy, validation_cost,\
             validation_accuracy, test_cost, test_accuracy = pickle.load(f)
-            converged = False
 
 while not converged and epoch < 500:
     epoch+=1
-    w, b = backpropagate_momentum_epoch(x_train, w, b, t_train, 0.1, 0.07, 50)
-
+    w, b = backpropagate_momentum_epoch(x_train, w, b, t_train, 0.05, 0.25, 50)
     train_cost.append(evaluate_cost(x_train, w, b, t_train))
     train_accuracy.append(evaluate_accuracy(x_train, w, b, t_train)[0])
 
@@ -59,20 +63,21 @@ while not converged and epoch < 500:
 
     test_cost.append(evaluate_cost(x_test, w, b, t_test))
     test_accuracy.append(evaluate_accuracy(x_test, w, b, t_test)[0])
-    #
-    # if epoch > 2 and abs(train_cost[-1] - train_cost[-2]) < 1e-4:
-    #     converged = True
+    if epoch > 5 and (abs(train_cost[-1] - train_cost[-2]) < 1e-4 or validation_cost[-1] - validation_cost[-5] > 1e-4):
+        converged = True
 
     st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
     print "[%s] Epoch: %3d" % (st, epoch)
-    print "\tTraining Set:   Cost: %8.3f Accuracy: %5.2f%%" %(train_cost[-1], train_accuracy[-1]*100)
-    print "\tValidation Set: Cost: %8.3f Accuracy: %5.2f%%" %(validation_cost[-1], validation_accuracy[-1]*100)
-    print "\tTest Set:       Cost: %8.3f Accuracy: %5.2f%%\n\n" %(test_cost[-1], test_accuracy[-1]*100)
+    print "\tTraining Set:   Cost: %8.4f Accuracy: %5.2f%%" %(train_cost[-1], train_accuracy[-1]*100)
+    print "\tValidation Set: Cost: %8.4f Accuracy: %5.2f%%" %(validation_cost[-1], validation_accuracy[-1]*100)
+    print "\tTest Set:       Cost: %8.4f Accuracy: %5.2f%%\n\n" %(test_cost[-1], test_accuracy[-1]*100)
 
     with open('logistic_regression.pickle', 'w') as f:
         pickle.dump([w, b, epoch, converged, train_cost, train_accuracy, validation_cost,\
                      validation_accuracy, test_cost, test_accuracy], f)
 
+with open("results/part5.csv", 'w') as fl:
+    fl.write("%.4f, %4.2f" %(test_cost[-1], test_accuracy[-1]*100))
 # Cost History Graph
 plt.figure()
 plt.plot(range(epoch), train_cost, label='Train Set')
@@ -126,14 +131,9 @@ print("\n--------------\nRUNNING PART 6\n--------------")
 visualize_weights(w, plot, ext)
 
 
-print("\n--------------\nRUNNING PART 8\n--------------")
-x, y, w, b = x_test, t_test, [np.zeros((784,300)), np.zeros((300,10))], [np.random.rand(300), np.random.rand(10)]
-compare_gradient2(x[0:10], w, b, y[0:10])
-
-
 print("\n--------------\nRUNNING PART 9\n--------------")
 epoch = 0
-
+w,b = [np.zeros((784,300)), np.zeros((300,10))], [np.random.rand(300), np.random.rand(10)]
 train_cost, train_accuracy = [], []
 validation_cost, validation_accuracy = [], []
 test_cost, test_accuracy = [], []
@@ -147,36 +147,36 @@ if os.path.exists('neural_network.pickle'):
             validation_accuracy, test_cost, test_accuracy = pickle.load(f)
 
 while not converged and epoch < 500:
+    w, b = backpropagate_momentum_epoch2(x_train, w, b, t_train, 0.01, 0.05, 50)
+    if epoch % 5 == 0:
+        train_cost.append(evaluate_cost2(x_train, w, b, t_train))
+        train_accuracy.append(evaluate_accuracy2(x_train, w, b, t_train)[0])
+
+        validation_cost.append(evaluate_cost2(x_validation, w, b, t_validation))
+        validation_accuracy.append(evaluate_accuracy2(x_validation, w, b, t_validation)[0])
+
+        test_cost.append(evaluate_cost2(x_test, w, b, t_test))
+        test_accuracy.append(evaluate_accuracy2(x_test, w, b, t_test)[0])
+        if epoch > 10 and (abs(train_cost[-1] - train_cost[-2]) < 1e-4 or validation_cost[-1] - validation_cost[-2] > 1e-3):
+            converged = True
+
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
+        print "[%s] Epoch: %3d" % (st, epoch+1)
+        print "\tTraining Set:   Cost: %8.4f Accuracy: %5.2f%%" %(train_cost[-1], train_accuracy[-1]*100)
+        print "\tValidation Set: Cost: %8.4f Accuracy: %5.2f%%" %(validation_cost[-1], validation_accuracy[-1]*100)
+        print "\tTest Set:       Cost: %8.4f Accuracy: %5.2f%%\n\n" %(test_cost[-1], test_accuracy[-1]*100)
     epoch+=1
-    w, b = backpropagate_momentum_epoch2(x_train, w, b, t_train, 0.01, 0.005, 50)
-
-    train_cost.append(evaluate_cost2(x_train, w, b, t_train))
-    train_accuracy.append(evaluate_accuracy2(x_train, w, b, t_train)[0])
-
-    validation_cost.append(evaluate_cost2(x_validation, w, b, t_validation))
-    validation_accuracy.append(evaluate_accuracy2(x_validation, w, b, t_validation)[0])
-
-    test_cost.append(evaluate_cost2(x_test, w, b, t_test))
-    test_accuracy.append(evaluate_accuracy2(x_test, w, b, t_test)[0])
-
-    # if epoch > 2 and abs(train_cost[-1] - train_cost[-2]) < 1e-4:
-    #     converged = True
-
-    st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
-    print "[%s] Epoch: %3d" % (st, epoch)
-    print "\tTraining Set:   Cost: %8.3f Accuracy: %5.2f%%" %(train_cost[-1], train_accuracy[-1]*100)
-    print "\tValidation Set: Cost: %8.3f Accuracy: %5.2f%%" %(validation_cost[-1], validation_accuracy[-1]*100)
-    print "\tTest Set:       Cost: %8.3f Accuracy: %5.2f%%\n\n" %(test_cost[-1], test_accuracy[-1]*100)
-
     with open('neural_network.pickle', 'w') as f:
         pickle.dump([w, b, epoch, converged, train_cost, train_accuracy, validation_cost,\
                      validation_accuracy, test_cost, test_accuracy], f)
 
+with open("results/part9.csv", 'w') as fl:
+    fl.write("%.4f, %4.2f" %(test_cost[-1], test_accuracy[-1]*100))
 # Cost History Graph
 plt.figure()
-plt.plot(range(epoch), train_cost, label='Train Set')
-plt.plot(range(epoch), validation_cost, label='Validation Set')
-plt.plot(range(epoch), test_cost, label='Test Set')
+plt.plot(np.array(range(epoch/5))*5, train_cost, label='Train Set')
+plt.plot(np.array(range(epoch/5))*5, validation_cost, label='Validation Set')
+plt.plot(np.array(range(epoch/5))*5, test_cost, label='Test Set')
 plt.legend(loc='best')
 plt.grid()
 plt.title('Cost History')
@@ -187,9 +187,9 @@ plt.show() if plot else plt.close()
 
 # Accuracy History Graph
 plt.figure()
-plt.plot(range(epoch), train_accuracy, label='Train Set')
-plt.plot(range(epoch), validation_accuracy, label='Validation Set')
-plt.plot(range(epoch), test_accuracy, label='Test Set')
+plt.plot(np.array(range(epoch/5))*5, train_accuracy, label='Train Set')
+plt.plot(np.array(range(epoch/5))*5, validation_accuracy, label='Validation Set')
+plt.plot(np.array(range(epoch/5))*5, test_accuracy, label='Test Set')
 plt.legend(loc='best')
 plt.grid()
 plt.title('Accuracy History')
@@ -220,8 +220,28 @@ for i, ax in enumerate(axes.flat):
 plt.savefig('results/part9_failures.%s' %(ext), bbox_inches='tight')
 plt.show() if plot else plt.close()
 
+
+print("\n--------------\nRUNNING PART 8\n--------------")
+compare_gradient2(x_test[:10], w, b, t_test[:10])
+
+
 print("\n--------------\nRUNNING PART 10\n--------------")
 wis = visualize_weights(w[0], plot, ext, 10)
 for i, wi in enumerate(wis):
     print "Weights Associated with",i
     print w[1][wi]
+
+with open('results/part10.out', 'w') as fl:
+    for i, wi in enumerate(wis):
+        fl.write("Weights Associated with %d\n"%(i))
+        fl.write("%s\n"%(w[1][wi]))
+
+
+print("---------------\nCOMPILING REPORT\n--------------")
+subprocess.call(['pdflatex', 'digits.tex'])
+subprocess.call(['pdflatex', 'digits.tex'])
+if sys.platform == "win32":
+    os.startfile('digits.pdf')
+else:
+    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, 'report.pdf'])
