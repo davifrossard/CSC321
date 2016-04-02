6a0dc7c812c182aa68e78ae6b03ad5cc92f5cd46
Davi Frossard <davienf1@gmail.com>
2016-04-02 02:21:36 -0400

    Implemented functions to print colored text

---
+++ Assignment 4/utils.py
@@ -0,0 +1,11 @@
+def print_warning(message):
+    print "\033[93m%s\033[0m" %message
+
+def print_success(message):
+    print "\033[92m%s\033[0m" %message
+
+def print_failure(message):
+    print "\033[91m%s\033[0m" %message
+
+def print_info(message):
+    print "\033[94m%s\033[0m" %message
